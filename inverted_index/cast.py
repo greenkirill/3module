4b2c84283e802e8499cc1c857add78ef1de3f35d
Gribkov, Kirill <kirill.gribkov@arcadia.spb.ru>
2018-11-21 20:34:22 +0300

    delta elias

--- inverted_index/cast.py
+++ inverted_index/cast.py
@@ -2,12 +2,16 @@ from settings import ENCODING, INDEX_LENGTH
 from v1 import v1
 from v2 import v2
 from v3 import v3
+from v4 import v4
 
 def v1row_to_v2row(v1row):
     return v2.kv_to_row(v1.row_to_kv(v1row))
 
 def v2row_to_v3row(v1row, intbytes=2, index_length=INDEX_LENGTH):
     return v3.kv_to_row(v2.row_to_kv(v1row), intbytes=intbytes, index_length=index_length)
+
+def v2row_to_v4row(v1row, intbytes=2, index_length=INDEX_LENGTH):
+    return v4.kv_to_row(v2.row_to_kv(v1row), intbytes=intbytes, index_length=index_length)
 
 
 def v1_to_v2(v1file, v2file):
@@ -32,6 +36,18 @@ def v2_to_v3(v2file, v3file, intbytes=3, index_length=INDEX_LENGTH):
             i = 0
             for l in rf.readlines():
                 r = v2row_to_v3row(l, intbytes=intbytes, index_length=index_length)
+                wf.write(r)
+                print(i)
+                i+=1
+
+def v2_to_v4(v2file, v3file, intbytes=3, index_length=INDEX_LENGTH):
+    with open(v2file, "r", encoding=ENCODING) as rf:
+        with open(v3file, "wb") as wf:
+            wf.write(v3.int_to_bytes(intbytes, intbytes=2))
+            wf.write(v3.int_to_bytes(index_length, intbytes=2))
+            i = 0
+            for l in rf.readlines():
+                r = v2row_to_v4row(l, intbytes=intbytes, index_length=index_length)
                 wf.write(r)
                 print(i)
                 i+=1
---
+++ inverted_index/v4.py
@@ -0,0 +1,84 @@
+from settings import ENCODING, INDEX_LENGTH
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.snowball import RussianStemmer
+import time
+from v3 import v3
+
+
+class v4:
+
+    def __init__(self, filename, mp, loadf=True, encoding=ENCODING, lang="ru"):
+        pass
+
+    @staticmethod
+    def kv_to_row(kv, lang="ru", encoding=ENCODING, intbytes=4, byteorder="big", index_length=INDEX_LENGTH):
+        wordbytes = v3.index_length(
+            lang=lang, encoding=encoding, index_length=index_length)
+        rw = bytes(kv[0], encoding=encoding)
+        rw += bytes(bytearray(wordbytes-len(rw)))
+
+        vls = b''
+
+        s = ""
+        for i in kv[1]:
+            s += v4.int_to_edelta(i)
+        N = len(s)//8
+        vls += v4.int_to_bytes(N+1,
+                               intbytes=intbytes, byteorder=byteorder)
+        f = 0
+        t = 8
+        for i in range(N):
+            vls += int(s[f:t], 2).to_bytes(1, "big")
+            f += 8
+            t += 8
+        n = len(s) - N*8
+        if n != 0:
+            print(s[f:t], n)
+            vls += int(s[f:t] + "0"*(8-n), 2).to_bytes(1, "big")
+        return rw + vls
+
+    @staticmethod
+    def int_to_edelta(i):
+        ln = 0
+        lengthOfLen = 0
+        temp = i
+        res = ""
+        while temp > 0:
+            ln += 1
+            temp >>= 1
+        temp = ln
+        while temp > 1:
+            lengthOfLen += 1
+            temp >>= 1
+        res = "0" * (lengthOfLen)
+        for j in range(lengthOfLen, -1, -1):
+            res += str((ln >> j) & 1)
+        for j in range(ln-2, -1, -1):
+            res += str((i >> j) & 1)
+        return res
+
+    @staticmethod
+    def int_to_bytes(i, intbytes=4, byteorder="big"):
+        return (i).to_bytes(intbytes, byteorder=byteorder)
+
+    @staticmethod
+    def row_to_kv(row, lang="ru", encoding=ENCODING, intbytes=4, byteorder="big", index_length=INDEX_LENGTH):
+        # wordbytes = v3.index_length(
+        #     lang=lang, encoding=encoding, index_length=index_length)
+        # key = row[:wordbytes]
+        # for i in range(wordbytes-1, -1, -1):
+        #     if key[i] != 0:
+        #         key = key[:i+1]
+        #         break
+        # n = v3.bytes_to_int(
+        #     row[wordbytes:wordbytes+intbytes], byteorder=byteorder)
+        # f = wordbytes+intbytes
+        # t = f+intbytes
+        # values = []
+        # for i in range(n):
+        #     values.append(int.from_bytes(row[f:t], byteorder=byteorder))
+        #     t += intbytes
+        #     f += intbytes
+        # return (key.decode(encoding), values)
+        pass
